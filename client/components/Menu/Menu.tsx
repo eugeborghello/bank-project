79d1c04e1dc02d260083976ec724d49ac49bf3db
Tatiana Vazquez <tatianavazquezr@hotmail.com>
2021-03-10 19:12:55 -0300

    commit

---
+++ client/components/Menu/Menu.tsx
@@ -0,0 +1,13 @@
+/* 
+Saldo actual en pesos de mi cuenta Henry Bank
+● Sumatoria General de Ingresos y Gastos de mi cuenta Henry
+Bank
+● Selector de periodo de sumatoria de ingresos y gastos de mi
+cuenta
+● Botón de Transacciones
+● Boton de Estadisticas
+● Boton de Mis Datos
+● Botón de Mis Productos
+● Botón de Recargar Dinero
+● Boton de Mandar Dinero
+*/

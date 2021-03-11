b5e0e1eacb3ad014a0fb9a5c6d3ecb5ec2036d3d
realeric23 <ericandresm@yahoo.com>
2021-03-11 02:44:11 -0300

    reset component

---
+++ client/src/components/Reset.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native';
+
+export const Reset = () => {
+    return (
+    <View >
+     
+        <Text>sdsdsd</Text>
+      
+    </View>
+    )
+}

bc03cce4dbe0a9c9ac5c81eed51244ba22dc953e
realeric23 <ericandresm@yahoo.com>
2021-03-11 23:59:24 -0300

    frontend update

--- client/App.tsx
+++ client/App.tsx
@@ -1,11 +1,48 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Reset } from './src/components/Reset';
+import AppLoading from 'expo-app-loading';
+import {
+  useFonts,
+  Roboto_100Thin,
+  Roboto_100Thin_Italic,
+  Roboto_300Light,
+  Roboto_300Light_Italic,
+  Roboto_400Regular,
+  Roboto_400Regular_Italic,
+  Roboto_500Medium,
+  Roboto_500Medium_Italic,
+  Roboto_700Bold,
+  Roboto_700Bold_Italic,
+  Roboto_900Black,
+  Roboto_900Black_Italic,
+} from '@expo-google-fonts/roboto';
+
+
 
 export default function App() {
+  let [fontsLoaded] = useFonts({
+    Roboto_100Thin,
+    Roboto_100Thin_Italic,
+    Roboto_300Light,
+    Roboto_300Light_Italic,
+    Roboto_400Regular,
+    Roboto_400Regular_Italic,
+    Roboto_500Medium,
+    Roboto_500Medium_Italic,
+    Roboto_700Bold,
+    Roboto_700Bold_Italic,
+    Roboto_900Black,
+    Roboto_900Black_Italic,
+  });
+
+  if (!fontsLoaded) return <AppLoading />;
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text  style={styles.titleText}>Hello World</Text>
+      <Reset />
       <StatusBar style="auto" />
     </View>
   );
@@ -17,5 +54,12 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    fontFamily: 'Roboto_700Bold'
+  },
+  titleText: {
+    fontSize: 24,
+    fontWeight: '600',
+    color: 'black',
+    fontFamily: 'Roboto_100Thin'
   },
 });
--- client/src/components/Reset.tsx
+++ client/src/components/Reset.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import { StyleSheet, Text, View } from 'react-native';
 
+
 export const Reset = () => {
     return (
     <View >
      
-        <Text>sdsdsd</Text>
+                                    <Text>erisdsds </Text>
       
     </View>
     )
 }
+

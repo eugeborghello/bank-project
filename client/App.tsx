77bd3a8a82c37e068fe5d637a82284bc0091f159
realeric23 <ericandresm@yahoo.com>
2021-03-13 13:55:37 -0300

    Merge branch 'development' into feReset
    Co-authored-by: florgranucci <florgranucci@users.noreply.github.com>

--- client/App.tsx
+++ client/App.tsx
@@ -1,83 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { SafeAreaView, StyleSheet, Text, View, Image } from 'react-native';
-import { Reset } from './src/components/Reset';
-import AppLoading from 'expo-app-loading';
-import colors from './assets/colors/colors';
-import * as PasswordReset from "./PasswordReset.png";
-import AppIntroSlider from 'react-native-app-intro-slider';
-import Icon from 'react-native-vector-icons/Entypo';
-
-Icon.loadFont();
-
-
-import {
-  useFonts,
-  Roboto_100Thin,
-  Roboto_100Thin_Italic,
-  Roboto_300Light,
-  Roboto_300Light_Italic,
-  Roboto_400Regular,
-  Roboto_400Regular_Italic,
-  Roboto_500Medium,
-  Roboto_500Medium_Italic,
-  Roboto_700Bold,
-  Roboto_700Bold_Italic,
-  Roboto_900Black,
-  Roboto_900Black_Italic,
-} from '@expo-google-fonts/roboto';
-
-
+import Reset  from './src/components/Reset';
+import { StyleSheet, View } from 'react-native';
 
 export default function App() {
-  let [fontsLoaded] = useFonts({
-    Roboto_100Thin,
-    Roboto_100Thin_Italic,
-    Roboto_300Light,
-    Roboto_300Light_Italic,
-    Roboto_400Regular,
-    Roboto_400Regular_Italic,
-    Roboto_500Medium,
-    Roboto_500Medium_Italic,
-    Roboto_700Bold,
-    Roboto_700Bold_Italic,
-    Roboto_900Black,
-    Roboto_900Black_Italic,
-  });
-
-  if (!fontsLoaded) return <AppLoading />;
-
-  const data = [
-  
-  {
-      title: 'FORGOT PASSWORD?',
-      text: "Don’t worry! It happens. Please enter the email address associated with your account. You will receive an email with instructions.",
-      // image: require('../../assets/3.jpg'),
-      bg: '#22bcb5',
-    },
-  ];
-  
-const renderItem = ({item}) => {
-return (
-  <Text>Reset Password</Text>
-)
-}
-
-const keyExtractor = (item) => item.title;
-
   return (
-    
-    <View style={{flex: 1}}>
-    <StatusBar translucent backgroundColor="transparent" />
-    <Image source={require("./images/PasswordReset.png")} />
-    <Icon name='email' size={15} color='black' />
-    <AppIntroSlider
-      keyExtractor={keyExtractor}
-      renderItem={renderItem}
-      data={data}
-    />
-  </View>
-    
+    <View style={styles.container}>
+      <Reset />
+    </View>
   );
 }
 
@@ -87,12 +16,6 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
-    fontFamily: 'Roboto_700Bold'
-  },
-  tinyLogo: {
-    fontSize: 25,
-    fontWeight: '600',
-    color: colors.textLink,
-    fontFamily: 'Roboto_100Thin'
   },
 });
+
--- client/assets/colors/colors.js
+++ client/assets/colors/colors.js
@@ -1,9 +1,12 @@
-export default colors = {
+const colors = {
     background: '#FFFFFF',
     textGreyLight: '#E6E6E6',
     textLight: '#3F3D56',
+    textInput: 'grey',
     textDark: '#000000',
     textLink: '#4A47A3',
     buttonGreyLight: '#E6E6E6',
     buttonViolet: '#413C69',
 };
+
+export default colors;

cc857b9632f6622e54652f13446153ff03a7aebf
realeric23 <ericandresm@yahoo.com>
2021-03-17 14:35:09 -0300

    sdsds

--- client/App.tsx
+++ client/App.tsx
@@ -1,11 +1,43 @@
 import React from 'react';
 import Nav from './components/Nav/Nav';
 import { StyleSheet, View } from 'react-native';
+import AppLoading from 'expo-app-loading';
+
+import {
+  useFonts,
+  Roboto_100Thin,
+  Roboto_100Thin_Italic,
+  Roboto_300Light,
+  Roboto_300Light_Italic,
+  Roboto_400Regular,
+  Roboto_400Regular_Italic,
+  Roboto_500Medium,
+  Roboto_500Medium_Italic,
+  Roboto_700Bold,
+  Roboto_700Bold_Italic,
+  Roboto_900Black,
+  Roboto_900Black_Italic,
+} from '@expo-google-fonts/roboto';
 
 export default function App() {
-  return (
-      <Nav/>
-  );
+  let [fontsLoaded] = useFonts({
+    Roboto_100Thin,
+    Roboto_100Thin_Italic,
+    Roboto_300Light,
+    Roboto_300Light_Italic,
+    Roboto_400Regular,
+    Roboto_400Regular_Italic,
+    Roboto_500Medium,
+    Roboto_500Medium_Italic,
+    Roboto_700Bold,
+    Roboto_700Bold_Italic,
+    Roboto_900Black,
+    Roboto_900Black_Italic,
+  });
+
+  if (!fontsLoaded) return <AppLoading />;
+
+  return <Nav />;
 }
 
 const styles = StyleSheet.create({

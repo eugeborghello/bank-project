e6183f4b6b42a5c70873d3893e78ba3b9a5760a3
Matias Lamela <matiaslamela1992@gmail.com>
2021-03-19 16:38:43 -0300

    one commit

--- server/src/controllers/users.js
+++ server/src/controllers/users.js
@@ -1,11 +1,11 @@
-const mongoose = require("mongoose");
-const Users = mongoose.model("Users")
+
+const mongoose = require('mongoose');
+const Users = mongoose.model('Users');
+const bcrypt = require('bcrypt');
+
 var nodemailer = require('nodemailer');
 const { generateCode } = require('../../utils/codeGen');
 const jwt = require('jsonwebtoken')
-
-
-
 
  exports.getUsers = (req, res) => {
     Users.find()
@@ -24,8 +24,7 @@ exports.getUserId=(req, res) => {
 }
 
 
-<<<<<<< HEAD
-=======
+
  
  exports.postLogin= async (req, res )=> {
    try{
@@ -66,8 +65,7 @@ exports.createUser=(req, res) => {
     .then(user => res.status(200).json({status:"success", response:user}))
     .catch(error => res.status(400).json({status:"error", message:error.message}))
 }
-  
->>>>>>> 71feaac2cb874887e62057c3e0edb4d598d9ab9e
+
 exports.updateDataUser=(req, res) => {
      const {id} = req.params;
     const cambios = req.body;
@@ -80,92 +78,89 @@ exports.updateDataUser=(req, res) => {
 })
 }
 
-exports.sendEmail= (req, res) => {
-    const { subject, email } = req.body;
-  
-    const transporter = nodemailer.createTransport({
-      host: 'smtp.gmail.com',
-      port: 465,
-      secure: true,
-      auth: {
-        user: 'xtremerssports@gmail.com',
-        pass: 'Hola123456!',
-      },
+exports.sendEmail = (req, res) => {
+  const { name, email, code, date, subject } = req.body;
+  // const { code, email } = req.body;
+
+  const transporter = nodemailer.createTransport({
+    host: 'smtp.gmail.com',
+    port: 465,
+    secure: true,
+    auth: {
+      user: 'xtremerssports@gmail.com',
+      pass: 'Hola123456!',
+    },
+  });
+
+  transporter
+    .sendMail({
+      from: '"Veski" <veski@gmail.com>',
+      to: email,
+      subject: subject,
+      text: "Here's your code to reset your password: " + code,
+    })
+    .then((mail) => {
+      res.status(200).json({
+        message: 'Mail send success',
+        info: mail,
+      });
+    })
+    .catch((error) => {
+      res.status(404).json({
+        message: 'Mail not send ' + error,
+      });
     });
-  
-    transporter
-      .sendMail({
-        from: '"Veski" <veski@gmail.com>',
-        to: email,
-        subject: 'Password Reset',
-        text: "Here's your code to reset your password: ",
-      })
-      .then((mail) => {
-        res.status(200).json({
-          message: 'Mail send success',
-          info: mail,
-        });
-      })
-      .catch((error) => {
-        res.status(404).json({
-          message: 'Mail not send ' + error,
-        });
-      });    
-}
+};
 
+exports.emailCode = async (req, res) => {
+  try {
+    const { userEmail } = req.body;
 
-exports.emailCode= async (req, res) => {
-    try {
-      const { userEmail } = req.body;
-  
-      const userfind = await Users.findOneAndUpdate(
-        { email: userEmail },
-        { resetCode: generateCode(6) },
-        { new:true }
-      );
-      console.log('SOY------->', userfind);
-      if (userfind) {
-        res.status(200).json({
-          message: 'Code generated correctly',
-          user: userfind,
-        });
-      } else {
-        res.status(400).json({
-          message: 'User not found',
-        });
-      }
-    } catch (error) {
+    const userfind = await Users.findOneAndUpdate(
+      { email: userEmail },
+      { resetCode: generateCode(6) },
+      { new: true }
+    );
+    console.log('SOY------->', userfind);
+    if (userfind) {
+      res.status(200).json({
+        message: 'Code generated correctly',
+        user: userfind,
+      });
+    } else {
       res.status(400).json({
-        status: 'error',
-        message: error.message,
+        message: 'User not found',
       });
     }
+  } catch (error) {
+    res.status(400).json({
+      status: 'error',
+      message: error.message,
+    });
   }
+};
 
+exports.passwordReset = async (req, res) => {
+  try {
+    const { resetCode, userEmail, newPass } = req.body;
+    console.log('soy----->', resetCode);
 
-exports.passwordReset= async (req, res) => {
-    try {
-      const { resetCode, userEmail, newPass } = req.body;
-      console.log('soy----->', resetCode);
-  
-      const user = await Users.findOne({ email: userEmail });
-      if (user && user.resetCode === resetCode) {
-        user.password = newPass;
-        await user.save();
-        res.status(200).json({
-          message: 'Password Updated correctly',
-          user: user,
-        });
-      } else {
-        res.status(400).json({
-          message: 'User not found or reset code does not match',
-        });
-      }
-    } catch (error) {
+    const user = await Users.findOne({ email: userEmail });
+    if (user && user.resetCode === resetCode) {
+      user.password = newPass;
+      await user.save();
+      res.status(200).json({
+        message: 'Password Updated correctly',
+        user: user,
+      });
+    } else {
       res.status(400).json({
-        message: 'Error: ' + error,
+        message: 'User not found or reset code does not match',
       });
     }
+  } catch (error) {
+    res.status(400).json({
+      message: 'Error: ' + error,
+    });
   }
-
-
+};

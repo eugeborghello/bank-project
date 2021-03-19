019fc87d63d5f2b8a759177ec535cf98bdfa8dac
Euge Borghello <craeugeniaborghello@gmail.com>
2021-03-19 16:27:24 -0300

    Register Validation ok codigo limpio

--- client/screens/Register/Register.tsx
+++ client/screens/Register/Register.tsx
@@ -38,8 +38,6 @@ export default function Register() {
               /^(("[\w-\s]+")|([\w-]+(?:\.[\w-]+)*)|("[\w-\s]+")([\w-]+(?:\.[\w-]+)*))(@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$)|(@\[?((25[0-5]\.|2[0-4][0-9]\.|1[0-9]{2}\.|[0-9]{1,2}\.))((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\.){2}(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\]?$)/i
             );
             if (!pattern.test(datos.email)) 
-            // { 
-            //     return alert("Please enter a valid email address") }
             {
               setError("Please enter a valid email address.");
               return false;
@@ -73,12 +71,7 @@ export default function Register() {
         }
 
     }
-        
-        
-
-    
-
-
+       
 
     return (
         <View style={styles.registerForm}>

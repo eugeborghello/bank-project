729aad8c8ca3ddf05ed654a7840b57602be8bf68
realeric23 <ericandresm@yahoo.com>
2021-03-20 01:21:40 -0300

    fonts update

--- client/App.tsx
+++ client/App.tsx
@@ -1,21 +1,54 @@
-import React from 'react';
-import Nav from './components/Nav/Nav';
-import { StyleSheet} from 'react-native';
-import {Provider} from 'react-redux';
-import store from './redux/store';
+import React from "react";
+import Nav from "./components/Nav/Nav";
+import { StyleSheet } from "react-native";
+import { Provider } from "react-redux";
+import store from "./redux/store";
+import {
+	useFonts,
+	Roboto_100Thin,
+	Roboto_100Thin_Italic,
+	Roboto_300Light,
+	Roboto_300Light_Italic,
+	Roboto_400Regular,
+	Roboto_400Regular_Italic,
+	Roboto_500Medium,
+	Roboto_500Medium_Italic,
+	Roboto_700Bold,
+	Roboto_700Bold_Italic,
+	Roboto_900Black,
+	Roboto_900Black_Italic,
+} from "@expo-google-fonts/roboto";
+import AppLoading from "expo-app-loading";
 
 export default function App() {
-  return (
-    <Provider store={store}>
-      <Nav/>
-    </Provider>
-  );
+	let [fontsLoaded] = useFonts({
+		Roboto_100Thin,
+		Roboto_100Thin_Italic,
+		Roboto_300Light,
+		Roboto_300Light_Italic,
+		Roboto_400Regular,
+		Roboto_400Regular_Italic,
+		Roboto_500Medium,
+		Roboto_500Medium_Italic,
+		Roboto_700Bold,
+		Roboto_700Bold_Italic,
+		Roboto_900Black,
+		Roboto_900Black_Italic,
+	});
+
+	if (!fontsLoaded) return <AppLoading />;
+
+	return (
+		<Provider store={store}>
+			<Nav />
+		</Provider>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		alignItems: "center",
+		justifyContent: "center",
+	},
 });

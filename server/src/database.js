57434d737acd5e777b40e9a5ddeed29a63da9ae4
realeric23 <ericandresm@yahoo.com>
2021-03-23 17:01:40 -0300

    Mongo Atlas added + PORT CHANGE

--- server/src/database.js
+++ server/src/database.js
@@ -1,19 +1,21 @@
-const mongoose= require('mongoose');
+const mongoose = require('mongoose');
 require('dotenv').config();
 
-const host = process.env.MONGO_HOST;
-const database = process.env.MONGO_DB;
+// const host = process.env.MONGO_HOST;
+// const database = process.env.MONGO_DB;
 
-URI=(`mongodb://${host}/${database}`)
+// URI = `mongodb://${host}/${database}`;
 
-mongoose.connect(URI, {
-    useNewUrlParser:true,
-    useUnifiedTopology:true,
-    useCreateIndex:true,
-    useFindAndModify:false
-})
- .then(db => console.log('base de datos conectada'))
- .catch(err => console.log(err))
+const URI = process.env.MONGO_URI;
 
+mongoose
+  .connect(URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then((db) => console.log('base de datos conectada'))
+  .catch((err) => console.log(err));
 
- module.exports= mongoose;
+module.exports = mongoose;

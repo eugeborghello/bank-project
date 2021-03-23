4f5cee6c71be83a891103602ae0becc73d14df10
realeric23 <ericandresm@yahoo.com>
2021-03-23 00:41:55 -0300

    FIX

--- server/index.js
+++ server/index.js
@@ -1,21 +1,19 @@
 const express = require('express');
 const app = express();
-const morgan=require('morgan');
+const morgan = require('morgan');
 const cors = require('cors');
 const routes = require('./src/routes/index.js');
-require('dotenv').config()
+require('dotenv').config();
 // Base de datos
-require('./src/database')
-
+require('./src/database');
 
 // Middelwares
 app.use(cors());
 app.use(morgan('dev'));
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-
 
 // Rutas
 app.use('/', routes);
 
-app.listen(3001 || 3002,() =>  console.log('escuchando en el puerto 3001'))
+app.listen(3001, () => console.log('escuchando en el puerto 3001'));

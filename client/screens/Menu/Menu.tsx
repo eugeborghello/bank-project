f22b7b493b1d80f62d4e301c095e80c5d1449df3
realeric23 <ericandresm@yahoo.com>
2021-03-23 16:23:47 -0300

    fix1

--- client/screens/Menu/Menu.tsx
+++ client/screens/Menu/Menu.tsx
@@ -1,19 +1,11 @@
-<<<<<<< HEAD
-import React, {FC} from 'react';
-import { Text, View, Image, Alert, TouchableOpacity } from 'react-native';
-import styles from './MenuStyles';
-import { MaterialCommunityIcons, FontAwesome, Octicons } from '@expo/vector-icons';
-import { useFonts, CutiveMono_400Regular } from '@expo-google-fonts/cutive-mono';
-import { useNavigation } from '@react-navigation/native';
-import { useSelector } from 'react-redux';
-
-=======
-import React, { FC, useState } from "react";
+import React, { FC } from "react";
 import { Text, View, Image, Alert, TouchableOpacity } from "react-native";
 import styles from "./MenuStyles";
 import { MaterialCommunityIcons, FontAwesome, Octicons } from "@expo/vector-icons";
-import { useFonts, Roboto_500Medium, Roboto_400Regular } from "@expo-google-fonts/roboto";
->>>>>>> 1a7dfc1afbdc7654fdd5c5883e49bc59d57e797a
+import { useFonts, CutiveMono_400Regular } from "@expo-google-fonts/cutive-mono";
+import { useNavigation } from "@react-navigation/native";
+import { useSelector } from "react-redux";
+
 /* 
 interface Props {
     icon: string;
@@ -24,37 +16,37 @@ const Menu: FC <Props> = () => {}
 */
 
 const Menu: FC = () => {
-    const user = useSelector(state => state.user.currentUser);
-    const account = useSelector(state => state.accountsReducer)
-    console.log({'res':account})
-    console.log(user[0])
-    const navigation = useNavigation()
-let [fontsLoaded] = useFonts({
-    CutiveMono_400Regular,
-  });
+	const user = useSelector((state) => state.user.currentUser);
+	const account = useSelector((state) => state.accountsReducer);
+	console.log({ res: account });
+	console.log(user[0]);
+	const navigation = useNavigation();
+	let [fontsLoaded] = useFonts({
+		CutiveMono_400Regular,
+	});
 
-  if (!fontsLoaded) {
-    return <View>
-        <Text>
-            Loading
-        </Text>
-    </View>;
-  } else {
-    return(
-        <View style={styles.view}>
-            <View style={styles.viewMenu} > 
-            <Text style={styles.text}> Hello, {user[0].name}! </Text>
-            <Image 
-            style={styles.image}
-            source={{
-                uri: 
-                'https://image.flaticon.com/icons/png/512/2936/2936469.png'
-}}/>
-            </View>
-            <View style={styles.viewBalance} >
-                <Text style={styles.balanceText} > Total balance:</Text>
-                <Text style={styles.balanceText} > ${account.balance||400} </Text> 
-            </View>
+	if (!fontsLoaded) {
+		return (
+			<View>
+				<Text>Loading</Text>
+			</View>
+		);
+	} else {
+		return (
+			<View style={styles.view}>
+				<View style={styles.viewMenu}>
+					<Text style={styles.text}> Hello, {user[0].name}! </Text>
+					<Image
+						style={styles.image}
+						source={{
+							uri: "https://image.flaticon.com/icons/png/512/2936/2936469.png",
+						}}
+					/>
+				</View>
+				<View style={styles.viewBalance}>
+					<Text style={styles.balanceText}> Total balance:</Text>
+					<Text style={styles.balanceText}> ${account.balance || 400} </Text>
+				</View>
 
 				<View style={styles.container}>
 					<View style={styles.viewCard}>
@@ -92,33 +84,31 @@ let [fontsLoaded] = useFonts({
 								<Text style={styles.buttonText}>Transactions</Text>
 							</TouchableOpacity>
 
-                <View style={styles.viewButtonRow}>
-                <TouchableOpacity 
-                    style={styles.button}
-                    onPress={() => navigation.navigate('CBU')}
-                    >
-                <MaterialCommunityIcons 
-                    name="account-cog" 
-                    size={20} 
-                    color="black" />
-                     <Text style={styles.buttonText}> 
-                        My account
-                    </Text>
-                </TouchableOpacity>
+							<View style={styles.viewButtonRow}>
+								<TouchableOpacity
+									style={styles.button}
+									onPress={() => navigation.navigate("CBU")}
+								>
+									<MaterialCommunityIcons
+										name="account-cog"
+										size={20}
+										color="black"
+									/>
+									<Text style={styles.buttonText}>My account</Text>
+								</TouchableOpacity>
 
-                <TouchableOpacity 
-                    style={styles.button}
-                    onPress={() => navigation.navigate('CreditCard')}
-                    >
-                <MaterialCommunityIcons 
-                    name="credit-card-multiple-outline" 
-                    size={20} 
-                    color="black" />
-                     <Text style={styles.buttonText}> 
-                        Cards
-                    </Text>
-                </TouchableOpacity>
-                </View>
+								<TouchableOpacity
+									style={styles.button}
+									onPress={() => navigation.navigate("CreditCard")}
+								>
+									<MaterialCommunityIcons
+										name="credit-card-multiple-outline"
+										size={20}
+										color="black"
+									/>
+									<Text style={styles.buttonText}>Cards</Text>
+								</TouchableOpacity>
+							</View>
 
 							<TouchableOpacity
 								style={styles.button}

b8f3eb484ea3c56b4a9f016a9711da28e7a62f43
Homero H <homero.herrero.92@gmail.com>
2021-03-24 15:12:38 -0300

    2

--- client/screens/Login/Login.tsx
+++ client/screens/Login/Login.tsx
@@ -14,6 +14,7 @@ import { REACT_APP_BACKEND_API_URL } from "@env";
 
 const Login = (props) => {
   const URL = `${REACT_APP_BACKEND_API_URL}/users`;
+  console.log(URL);
   const dispatch = useDispatch();
 
 	const [error, setError] = useState<string>("");

4cb3472350bf453406402326246314e0477a1c9c
Euge Borghello <craeugeniaborghello@gmail.com>
2021-03-25 19:48:17 -0300

    historial transacciones finished

--- client/screens/Historial/Historial.tsx
+++ client/screens/Historial/Historial.tsx
@@ -1,12 +1,121 @@
 import React, { useState } from 'react';
 import { Text, View, TextInput, TouchableOpacity, Image, Alert } from 'react-native';
+import { AntDesign } from '@expo/vector-icons';
+import { Link } from "@react-navigation/native";
 import styles from "./styles";
 
 const Historial = (props) => {
 
 
     return (
-        <View style={styles.header}>
+        <View style={styles.container}>
+            <View style={styles.back}>
+                <Link to="/Home">
+                <AntDesign name="arrowleft" size={24} color="black" />
+                </Link>
+            </View>
+
+            <View style={styles.title}>
+                <Text style={{ fontFamily: "Roboto_700Bold", color: '#413C69', fontSize: 30,}}> Transactions history </Text>
+            </View>
+            
+            <View style={styles.header}>
+                <View style={styles.headerLeft}>
+                <Text style={{fontFamily: "Roboto_400Regular", color: "#4A47A3",  fontSize: 15,}}> Recent: </Text>
+                </View>
+
+                <View style={styles.headerRight}>
+                <Text style={{fontFamily: "Roboto_400Regular", color: "#4A47A3",  fontSize: 15,}}> Statistics </Text>
+                </View>
+            </View>
+            <View style={styles.card}> 
+                    <View style={styles.iconTextContainer}>
+                        <TouchableOpacity  
+                            style={styles.icon}>  
+                            <AntDesign name="arrowup" size={24} color="black" />    
+
+                            <View style={styles.text}>
+                                <Text style={styles.cardText}> Henry </Text>
+                                <Text style={styles.cardText2}> Today 15.30hs </Text>
+                            </View>             
+                        </TouchableOpacity>
+                        
+                    </View>
+
+                    <View style={styles.amount}> 
+                        <Text style={{fontFamily: "Roboto_500Medium", color: "#4A47A3", }} > USD450 </Text>
+                    </View>
+                    
+                </View>
+
+                <View style={styles.card}> 
+                    <View style={styles.iconTextContainer}>
+                        <TouchableOpacity  
+                            style={styles.icon}>  
+                            <AntDesign name="arrowup" size={24} color="black" />    
+
+                            <View style={styles.text}>
+                                <Text style={styles.cardText}> Henry </Text>
+                                <Text style={styles.cardText2}> Today 11.30hs </Text>
+                            </View>             
+                        </TouchableOpacity>
+                        
+                    </View>
+
+                    <View style={styles.amount}> 
+                        <Text style={{fontFamily: "Roboto_500Medium", color: "#4A47A3", }} > USD150 </Text>
+                    </View>
+                    
+                </View>
+            
+
+                <View style={styles.card}> 
+                    <View style={styles.iconTextContainer}>
+                        <TouchableOpacity  
+                            style={styles.icon}>  
+                            <AntDesign name="arrowup" size={24} color="black" />    
+
+                            <View style={styles.text}>
+                                <Text style={styles.cardText}> Henry </Text>
+                                <Text style={styles.cardText2}> Yesterday 11.30hs </Text>
+                            </View>             
+                        </TouchableOpacity>
+                        
+                    </View>
+
+                    <View style={styles.amount}> 
+                        <Text style={{fontFamily: "Roboto_500Medium", color: "#4A47A3", }} > USD50 </Text>
+                    </View>
+                    
+                </View>
+
+                <View style={styles.card}> 
+                    <View style={styles.iconTextContainer}>
+                        <TouchableOpacity  
+                            style={styles.icon}>  
+                            <AntDesign name="arrowup" size={24} color="black" />    
+
+                            <View style={styles.text}>
+                                <Text style={styles.cardText}> Henry </Text>
+                                <Text style={styles.cardText2}> Yesterday 10.00hs </Text>
+                            </View>             
+                        </TouchableOpacity>
+                        
+                    </View>
+
+                    <View style={styles.amount}> 
+                        <Text style={{fontFamily: "Roboto_500Medium", color: "#4A47A3", }} > USD45 </Text>
+                    </View>
+                    
+                </View>
+
+               
+                
+                
+
+           
+            
+
 
 
         </View>
--- client/screens/Historial/styles.tsx
+++ client/screens/Historial/styles.tsx
@@ -1,9 +1,93 @@
 import { StyleSheet } from "react-native";
+import { white } from "react-native-paper/lib/typescript/styles/colors";
 
 const styles = StyleSheet.create({
-    header: {
+    container: {
         width: '100%',
-        paddingLeft: 15
+        flex: 1,
+        flexDirection: 'column',
+        paddingLeft: 20,
+        paddingRight: 20,
+        backgroundColor: 'white',        
+    },
+    back: {
+        flex: 0.5,
+        backgroundColor: 'white',
+        color: 'white',
+        justifyContent: 'center',
+        marginTop: 10,
+    },
+    title: {
+        flex: 1.3,
+        backgroundColor: 'white',
+        justifyContent: 'center',
+        alignItems: 'center',
+       
+    },
+    header: {
+        flex: 0.5,
+        flexDirection: 'row',
+       
+    },
+    headerLeft: {
+        flex: 1,
+        alignItems: "flex-start",
+        
+    },
+    headerRight: {
+        flex: 1,
+        alignItems: "flex-end",
+        color: "#4A47A3",
+        fontSize: 20,
+        
+    },
+    bodyContainer: {
+        flex: 1,
+        
+    },
+    card: {
+        flex: 1,
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        borderColor: 'black',
+		backgroundColor: 'rgb(167, 197, 235)',
+        borderRadius: 10,
+        marginBottom: 10,
+
+    },
+    iconTextContainer: {
+		flexDirection: 'row',
+		justifyContent: 'center',
+        alignItems: 'center',
+	},
+    icon: {
+		height: 70,
+		flexDirection: 'row',
+        justifyContent: 'center',
+		alignItems: 'center'
+	},
+    text:{
+        flexDirection: 'column',
+
+    },
+    cardText: {
+		color: 'black',
+		fontSize: 14,
+		fontWeight: 'bold',
+		marginLeft: 3,
+        fontFamily: "Roboto_500Medium",
+	},
+    cardText2: {
+		color: 'black',
+		fontSize: 10,
+		marginLeft: 3,
+        paddingTop: 5,
+        fontFamily: "Roboto_300Light",
+	},
+    amount: {
+        
+
     },
     
 })

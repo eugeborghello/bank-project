959dd5825c130d7dba9fdd5b6d3035ee49781f9b
florgranucci <flor.g.24@outlook.com>
2021-03-26 16:10:33 -0300

    recharge 3

--- client/screens/RechargeMoney/Recharge.tsx
+++ client/screens/RechargeMoney/Recharge.tsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 import React, { useState, useContext } from "react";
-import { Text, View, Image, TextInput, TouchableOpacity, Alert } from "react-native";
+import { Text, View, Image, TextInput, TouchableOpacity, ScrollView } from "react-native";
 import styles from "./styles";
 import { useDispatch } from "react-redux";
 import HandleDrawer from "../../components/Nav/HandleDrawer";
@@ -9,17 +9,19 @@ import { Picker } from "@react-native-picker/picker";
 import { REACT_APP_BACKEND_API_URL } from "@env";
 
 const Login = () => {
-	const URL = `${REACT_APP_BACKEND_API_URL}/users`;
-	const dispatch = useDispatch();
-
-	const [selectedLanguage, setSelectedLanguage] = useState();
+	/* const URL = `${REACT_APP_BACKEND_API_URL}/users`;
+	const dispatch = useDispatch(); */
 
 	const [error, setError] = useState<string>("");
 	const [inputs, setInputs] = useState({
-		card: "",
+		cardNumber: "",
 		expire: "",
 		amount: "",
-		account: ""
+		account: "",
+		securityCode: "",
+		bank: "",
+		cardName: "",
+		type: "",
 	});
 
 	// input handlers
@@ -32,48 +34,97 @@ const Login = () => {
 	};
 
 	return (
-		<>
+		<ScrollView>
 			<View style={styles.root}>
 				<HandleDrawer />
 				<Text style={styles.title}>RECHARGE MONEY</Text>
 
-				<View style={styles.containerA}>
-					<Picker 
-					selectedValue={inputs.account}
-					onValueChange={(itemValue, itemIndex) => handleChange(itemValue, "account")}
+				<View style={styles.mercado}>
+					<Image
+						style={styles.mercadoLogo}
+						source={require("../../assets/images/mercado.png")}
+					/>
+				</View>
+
+				<View>
+					<Picker
+						selectedValue={inputs.account}
+						onValueChange={(itemValue, itemIndex) => handleChange(itemValue, "account")}
 					>
-					    
 						<Picker.Item label="US DOLLARS" value="US DOLLARS" />
 						<Picker.Item label="ARG PESO" value="ARG PESO" />
 					</Picker>
 				</View>
+				<View>
+					<Picker
+						selectedValue={inputs.bank}
+						onValueChange={(itemValue, itemIndex) => handleChange(itemValue, "bank")}
+					>
+						<Picker.Item label="BANCO NACION" value="BANCO NACION" />
+						<Picker.Item label="BANCO SANTANDER" value="BANCO SANTANDER" />
+						<Picker.Item label="BANCO GALICIA" value="BANCO GALICIA" />
+					</Picker>
+				</View>
+				<View>
+					<Picker
+						selectedValue={inputs.type}
+						onValueChange={(itemValue, itemIndex) => handleChange(itemValue, "type")}
+					>
+						<Picker.Item label="DNI" value="DNI" />
+						<Picker.Item label="CÉDULA" value="CÉDULA" />
+						<Picker.Item label="OTRO" value="OTRO" />
+					</Picker>
+				</View>
 
 				<View style={styles.container}>
-					<View>
-						<Text>Card number</Text>
+					<View style={styles.inputsCard}>
+						<Text>Card Name</Text>
 						<TextInput
-							placeholder="1234 5678 1234 5678"
-							style={styles.input}
-							onChangeText={(value) => handleChange(value, "card")}
-							value={inputs.card}
+							placeholder="APRO"
+							style={styles.inputs}
+							secureTextEntry={true}
+							onChangeText={(value) => handleChange(value, "cardName")}
+							value={inputs.cardName}
 						/>
 					</View>
-					<View>
+					<View style={styles.inputsCode}>
+						<Text>Security Code</Text>
+						<TextInput
+							placeholder="123"
+							style={styles.inputs}
+							onChangeText={(value) => handleChange(value, "securityCode")}
+							value={inputs.securityCode}
+						/>
+					</View>
+				</View>
+
+				<View style={styles.container}>
+					<View style={styles.inputsCard}>
+						<Text>Card Number</Text>
+						<TextInput
+							placeholder="5031 7557 3453 0604"
+							style={styles.inputs}
+							secureTextEntry={true}
+							onChangeText={(value) => handleChange(value, "cardNumber")}
+							value={inputs.cardNumber}
+						/>
+					</View>
+					<View style={styles.inputsCode}>
 						<Text>Expire date</Text>
 						<TextInput
 							placeholder="MM/YY"
-							style={styles.input}
+							style={styles.inputs}
 							onChangeText={(value) => handleChange(value, "expire")}
 							value={inputs.expire}
 						/>
 					</View>
 				</View>
-				<View style={styles.containerA}>
+
+				<View style={styles.amount}>
 					<Text>Amount</Text>
 					<TextInput
 						placeholder="$50"
-						style={styles.input}
-						secureTextEntry={true}
+						style={styles.inputAmount}
 						onChangeText={(value) => handleChange(value, "amount")}
 						value={inputs.amount}
 					/>
@@ -82,19 +133,13 @@ const Login = () => {
 
 				<Text>{error && error}</Text>
 
-				<View style={styles.mercado}>
-				<TouchableOpacity>
-					<Image style={styles.mercadoLogo} source={require("../../assets/images/mercado.png")} />
-				</TouchableOpacity>
-				</View>
-
-				<View style={styles.loginButton}>
+				<View style={styles.rechargeButton}>
 					<TouchableOpacity activeOpacity={0.2} onPress={handleConfirm}>
-						<Text style={styles.loginStyle}>Recharge</Text>
+						<Text style={styles.rechargeStyles}>Recharge</Text>
 					</TouchableOpacity>
 				</View>
 			</View>
-		</>
+		</ScrollView>
 	);
 };
 
--- client/screens/RechargeMoney/styles.tsx
+++ client/screens/RechargeMoney/styles.tsx
@@ -1,53 +1,70 @@
 import { StyleSheet } from "react-native";
 
 const styles = StyleSheet.create({
-  root: {
-    flexDirection: "column",
-    padding: 10,
-    marginTop: 30
-  },
-  title: {
-    fontSize: 30,
-    color: "#413C69",
-    fontWeight: "500",
-    marginBottom: 10,
-    alignSelf: "center",
-  },
-  container: {
-    flexDirection: 'row', 
-    width: '90%', 
-    justifyContent: 'space-evenly',
-    alignSelf: 'center',
-    marginVertical: 20
-  },
-  input: {
-    borderBottomColor: "grey",
-    borderBottomWidth: 1,
-  },
-  containerA: {
-    width: '70%', 
-    alignSelf: 'center'
-  },
-  loginButton: {
-    width: "80%",
-    marginTop: 10,
-    borderRadius: 7,
-    alignSelf: 'center',
-    backgroundColor: "#413C69",
-  },
-  loginStyle: {
-    color: "#FFFFFF",
-    margin: 8,
-    textAlign: "center",
-  },
-  mercado: {
-    alignSelf: 'center', 
-    marginVertical: 15
-  },
-  mercadoLogo: {
-    width: 320, 
-    height: 100
-  }
+	root: {
+		flexDirection: "column",
+		padding: 10,
+		marginTop: 30,
+		marginHorizontal: 30,
+	},
+	title: {
+		fontSize: 30,
+		color: "#413C69",
+		fontWeight: "500",
+		marginBottom: 10,
+		alignSelf: "center",
+	},
+	container: {
+		flexDirection: "row",
+		marginVertical: 10,
+	},
+
+	inputsCard: {
+		width: "60%",
+		padding: 5,
+	},
+	inputsCode: {
+		width: "40%",
+		padding: 5,
+	},
+
+	inputs: {
+		borderBottomColor: "grey",
+		borderBottomWidth: 1,
+		width: "100%",
+	},
+
+	amount: {
+		width: "100%",
+		padding: 5,
+	},
+
+	inputAmount: {
+		borderBottomColor: "grey",
+		borderBottomWidth: 1,
+		width: "40%",
+	},
+
+	rechargeButton: {
+		width: "100%",
+		marginVertical: 15,
+		borderRadius: 7,
+		alignSelf: "center",
+		backgroundColor: "#413C69",
+	},
+	rechargeStyles: {
+		color: "#FFFFFF",
+		margin: 8,
+		textAlign: "center",
+	},
+	mercado: {
+		alignSelf: "center",
+		marginVertical: 30,
+	},
+	mercadoLogo: {
+		width: 250,
+		height: 80,
+	},
 });
 
 export default styles;

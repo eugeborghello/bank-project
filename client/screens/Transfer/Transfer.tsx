ace307a6598985921ef82a7fda06ad908b2309ba
florgranucci <flor.g.24@outlook.com>
2021-03-28 20:38:08 -0300

    styles

--- client/screens/Transfer/Transfer.tsx
+++ client/screens/Transfer/Transfer.tsx
@@ -1,124 +1,181 @@
 import React, { FC, useState } from "react";
 import styles from "./TransferStyles";
-import { Text, View, Image, Alert, TouchableOpacity, ScrollView } from "react-native";
-import { FontAwesome, Ionicons } from '@expo/vector-icons';
-import { TextInput, Appbar } from 'react-native-paper';
-import { useNavigation } from '@react-navigation/native';
+import { Text, View, Image, Alert, TouchableOpacity, ScrollView, TextInput } from "react-native";
+import { Appbar, Avatar, Checkbox } from "react-native-paper";
+import { useNavigation } from "@react-navigation/native";
+import Dollar from "react-native-vector-icons/Fontisto";
+import Icon from "react-native-vector-icons/Fontisto";
 
+const Transfer: FC = () => {
+	const navigation = useNavigation();
 
+	const [checkbox, setCheckbox] = useState<string>("arg");
 
-const Transfer: FC = (props) => {
-    const navigation = useNavigation()
-    console.log(props)
+	const handleCheck = (v: string) => {
+		setCheckbox(v);
+		checkbox === "arg"
+			? Alert.alert("Account ARG peso selected")
+			: Alert.alert("Account US dollar selected");
+	};
 
-    const [transferCbu, setTransferCbu] = useState({
-        name: '',
-        key: '',
-        amount: '',
-        description: ''
-    });
-    const [contacts, setContacts] = useState([
-        { name: 'Sandra Gomez', key: '5', account: '234406877', image: 'https://iteragrow.com/wp-content/uploads/2018/04/buyer-persona-e1545248524290.jpg' },
-        { name: 'Jose Fernando', key: '6', account: '234406877', image: 'https://iteragrow.com/wp-content/uploads/2018/04/buyer-persona-e1545248524290.jpg' },
-        { name: 'Woody Allen', key: '7', account: '234406877', image: 'https://iteragrow.com/wp-content/uploads/2018/04/buyer-persona-e1545248524290.jpg' },
-        { name: 'Sandra Gomez', key: '8', account: '234406877', image: 'https://iteragrow.com/wp-content/uploads/2018/04/buyer-persona-e1545248524290.jpg' }
-    ])
+	const [transferCbu, setTransferCbu] = useState({
+		name: "",
+		key: "",
+		amount: "",
+		description: "",
+	});
+	const [contacts, setContacts] = useState([
+		{
+			name: "Sandra Gomez",
+			key: "5",
+			account: "234406877",
+			image:
+				"https://iteragrow.com/wp-content/uploads/2018/04/buyer-persona-e1545248524290.jpg",
+		},
+		{
+			name: "Jose Fernando",
+			key: "6",
+			account: "234406877",
+			image:
+				"https://iteragrow.com/wp-content/uploads/2018/04/buyer-persona-e1545248524290.jpg",
+		},
+		{
+			name: "Woody Allen",
+			key: "7",
+			account: "234406877",
+			image:
+				"https://iteragrow.com/wp-content/uploads/2018/04/buyer-persona-e1545248524290.jpg",
+		},
+		{
+			name: "Sandra Gomez",
+			key: "8",
+			account: "234406877",
+			image:
+				"https://iteragrow.com/wp-content/uploads/2018/04/buyer-persona-e1545248524290.jpg",
+		},
+	]);
 
-    const [account, setAccount] = useState('')
+	const handleChange = (value: string, name: string): void => {
+		setTransferCbu({ ...transferCbu, [name]: value });
+	};
 
-    const handleChange = (value: string, name: string): void => {
-        setTransferCbu({ ...transferCbu, [name]: value });
-    }
+	return (
+		<>
+			<Appbar.Header style={{ backgroundColor: "#4a47a3" }}>
+				<Appbar.BackAction onPress={() => navigation.navigate("Home")} />
+			</Appbar.Header>
 
+			<ScrollView>
+				<View style={styles.general}>
+					<Text style={styles.title}>TRANSFER</Text>
 
-    return (
-        <>
+					<Text style={styles.recentTitle}> Select a contact: </Text>
 
-            <Appbar.Header style={{ backgroundColor: "#4a47a3" }}>
-                <Appbar.BackAction onPress={() => navigation.navigate("Home")} />
-            </Appbar.Header>
-
-            <ScrollView>
-                <View style={styles.general}>
-
-
-                    <Text style={styles.title}>
-                        Transfer
-                    </Text>
-
-                    <View style={styles.recentGeneral}>
-                    </View>
-                    <Text style={styles.recentTitle}> Select a contac: </Text>
-
-                    <TouchableOpacity style={styles.sub2}>
-                        <Ionicons name="add" size={16} color="grey" />
-                        <Text style={styles.sub2Text}> Add</Text>
-                    </TouchableOpacity>
-
-                    <View style={styles.content}>
-                        {contacts.map((el) => {
-                            return (
-                                <TouchableOpacity style={styles.contentEl} onPress={() => handleChange(el.name, 'name')}
-                                    key={el.key}>
-                                    <Image source={{ uri: el.image }}
-                                        style={styles.contentElImg} />
-                                    <Text style={styles.contentElName}> {el.name}</Text>
-
-                                </TouchableOpacity>
-                            )
-                        })}
-                        <View style={styles.container}>
-                            <Text style={styles.titleTransfer}>Transfer to:</Text>
-                            <TextInput
-                                label="name"
-                                placeholder="name"
-                                onChangeText={(value) => handleChange(value, "name")}
-                                value={transferCbu.name}
-
-                            />
-
-                            <Text style={styles.titleTransfer}>Enter amount:</Text>
-                            <TextInput
-                                label="$"
-                                placeholder="-------"
-                                onChangeText={(value) => handleChange(value, "amount")}
-                                value={transferCbu.amount}
-                            />
-
-                            <TouchableOpacity style={styles.contentEl} onPress={() => Alert.alert('cuenta en pesos')}>
-                                <FontAwesome name="dollar" size={16} color="blue" />
-                                <Text style={styles.sub2Text}>ARG PESO</Text>
-                            </TouchableOpacity>
-
-                            <TouchableOpacity style={styles.contentEl} onPress={() => Alert.alert('cuenta en dollar')}>
-                                <FontAwesome name="dollar" size={16} color="blue" />
-                                <Text style={styles.sub2Text}>US DOLLAR</Text>
-                            </TouchableOpacity>
-                            <Text style={styles.titleTransfer}>Description:</Text>
-
-                            <TextInput
-                                label="Description"
-                                placeholder="Description"
-                                onChangeText={(value) => handleChange(value, "description")}
-                                value={transferCbu.description}
-                            />
-                            <View style={styles.containerButton}>
-                                <TouchableOpacity style={styles.button}
-                                    onPress={() => console.log('me hiciste un click')}
-                                >
-                                    <Text style={styles.btntext}>Confirm</Text>
-                                </TouchableOpacity>
-                            </View>
-
-                        </View>
-                    </View>
-
-                </View>
-
-            </ScrollView>
-        </>
-    )
-}
+					<View style={styles.contacts}>
+						<ScrollView horizontal={true}>
+							<View>
+								<TouchableOpacity>
+									<Avatar.Icon
+										style={styles.addButton}
+										icon="account-multiple-plus-outline"
+									/>
+								</TouchableOpacity>
+							</View>
+							<View style={styles.contacts}>
+								{contacts.map((el) => {
+									return (
+										<View>
+											<TouchableOpacity
+												onPress={() => handleChange(el.name, "name")}
+												key={el.key}
+											>
+												<Image
+													source={{ uri: el.image }}
+													style={styles.avatar}
+												/>
+												<Text style={styles.contactName}>{el.name}</Text>
+											</TouchableOpacity>
+										</View>
+									);
+								})}
+							</View>
+						</ScrollView>
+					</View>
+					<View style={styles.content}>
+						<View style={styles.inputsContainer}>
+							<View>
+								<Text style={styles.titleTransfer}>Transfer to:</Text>
+								<View style={styles.inputWrapper}>
+									<Icon
+										style={{ marginTop: 18 }}
+										name="person"
+										size={18}
+										color="grey"
+									/>
+									<TextInput
+										onChangeText={(value) => handleChange(value, "name")}
+										value={transferCbu.name}
+										style={styles.inputs}
+									/>
+								</View>
+							</View>
+							<View>
+								<Text style={styles.titleTransfer}>Enter amount:</Text>
+								<View style={styles.inputWrapper}>
+									<Dollar
+										style={{ marginTop: 18 }}
+										name="dollar"
+										size={18}
+										color="grey"
+									/>
+									<TextInput
+										onChangeText={(value) => handleChange(value, "amount")}
+										value={transferCbu.amount}
+										style={styles.inputs}
+									/>
+								</View>
+							</View>
+						</View>
+						<View style={styles.checkContainer}>
+							<Checkbox.Item
+								status={checkbox === "arg" ? "checked" : "unchecked"}
+								style={{ width: "100%" }}
+								color="#4a47a3"
+								label="ARG peso"
+								onPress={() => handleCheck("arg")}
+							/>
+							<Checkbox.Item
+								status={checkbox === "us" ? "checked" : "unchecked"}
+								style={{ width: "100%" }}
+								color="#4a47a3"
+								label="US dollars"
+								onPress={() => handleCheck("us")}
+							/>
+						</View>
+						<View style={styles.inputsContainer}>
+							<Text style={styles.titleTransfer}>Description:</Text>
+							<View style={styles.inputWrapper}>
+								<TextInput
+									placeholder="Rent..."
+									onChangeText={(value) => handleChange(value, "description")}
+									value={transferCbu.description}
+									style={styles.inputs}
+								/>
+							</View>
+						</View>
+						<View style={styles.containerButton}>
+							<TouchableOpacity
+								style={styles.button}
+								onPress={() => (Alert.alert('Transfer was successfully'))}
+							>
+								<Text style={styles.btntext}>Confirm</Text>
+							</TouchableOpacity>
+						</View>
+					</View>
+				</View>
+			</ScrollView>
+		</>
+	);
+};
 
 export default Transfer;
-
--- client/screens/Transfer/TransferStyles.tsx
+++ client/screens/Transfer/TransferStyles.tsx
@@ -1,113 +1,96 @@
 import { StyleSheet } from "react-native";
 
 const styles = StyleSheet.create({
-    general: {
-        top: 20,
-        flexDirection: 'column',
-        justifyContent: 'space-evenly',
-        alignContent: 'center'
-    },
-    back: {
-        marginLeft: '5%',
-        top: 20
-    },
-    title: {
-        color: 'rgb(65, 60, 105)',
-        fontSize: 26,
-        fontWeight: 'bold',
-        alignSelf: 'flex-start',
-        top: 40
-    },
-    recentGeneral: {
-        top: 40
-    },
-    recentTitle: {
-        color: 'rgb(65, 60, 105)',
-        fontSize: 16,
-        fontWeight: 'bold',
-        marginLeft: '5%'
-    },
-    content: {
-        flexDirection: 'column',
-        justifyContent: 'space-evenly'
+	general: {
+		marginVertical: 20,
+		flexDirection: "column",
+		padding: 30,
+	},
+	title: {
+		color: "rgb(65, 60, 105)",
+		fontSize: 26,
+		fontWeight: "bold",
+		alignSelf: "center",
+	},
+	recentTitle: {
+		color: "rgb(65, 60, 105)",
+		fontSize: 16,
+		fontWeight: "bold",
+		marginVertical: 15,
+	},
+	contacts: {
+		flexDirection: "row",
+		alignItems: "center",
+	},
+	avatar: {
+		marginTop: 10,
+		height: 60,
+		width: 60,
+		borderRadius: 50,
+		marginHorizontal: 8,
+	},
+	contactName: {
+		alignSelf: "center",
+		width: "70%",
+		top: 15,
+	},
+	content: {
+		flexDirection: "column",
+		justifyContent: "space-evenly",
+	},
+	contentEl: {
+		flexDirection: "row",
+		justifyContent: "space-evenly",
+		alignContent: "center",
+		color: "#FFFFFF",
+	},
+	addButton: {
+		height: 60,
+		width: 60,
+		top: 10,
+		borderRadius: 50,
+		backgroundColor: "#4a47a3",
+	},
+	inputsContainer: {
+		alignSelf: "center",
+		marginTop: 40,
+		width: "80%",
+	},
+	inputs: {
+		borderBottomColor: "grey",
+		borderBottomWidth: 1,
+		marginVertical: 15,
+		marginLeft: 6,
+		width: "100%",
+	},
+	inputWrapper: {
+		flexDirection: "row",
+		marginVertical: 5,
+	},
+	titleTransfer: {
+		color: "rgb(65, 60, 105)",
+		fontSize: 16,
+		fontWeight: "bold",
+	},
+	checkContainer: {
+		width: "80%",
+		alignSelf: "center",
+	},
+	button: {
+		width: "100%",
+		marginTop: 10,
+		borderRadius: 7,
+		backgroundColor: "#413C69",
+	},
+	containerButton: {
+		width: "80%",
+		alignSelf: "center",
+	},
+	btntext: {
+		color: "#FFFFFF",
+		margin: 8,
+		textAlign: "center",
+	},
+});
 
-    },
-    contentEl: {
-        flexDirection: 'column',
-        justifyContent: 'space-evenly',
-        alignContent: 'center',
-        color: "#FFFFFF",
-    },
-    contentElImg: {
-        height: 60,
-        width: 60,
-        top: 10,
-        borderRadius: 50
-    },
-    contentElName: {
-        alignSelf: 'center',
-        top: 15
-    },
-    subtitles: {
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        top: 90
-    },
-    sub1: {
-        marginLeft: '5%',
-        fontWeight: 'bold',
-        fontSize: 14.5,
-        color: 'grey'
-    },
-    sub2: {
-        flexDirection: 'row'
-    },
-    sub2Text: {
-        fontSize: 14.5,
-        color: 'grey',
-        marginRight: '10%'
-    },
-    container: {
-        flexDirection: 'column',
-        justifyContent: 'space-between',
-        alignSelf: 'center',
-        top: 100
-    },
-
-
-    name: {
-        fontSize: 18,
-        fontWeight: 'bold'
-    },
-    account: {
-        color: 'grey',
-        fontSize: 14.5
-    },
-
-    titleTransfer: {
-        color: 'rgb(65, 60, 105)',
-        fontSize: 16,
-        fontWeight: 'bold',
-        marginLeft: '5%',
-        justifyContent: 'space-evenly'
-    },
-    button: {
-        width: "100%",
-        marginTop: 10,
-        borderRadius: 7,
-        backgroundColor: "#413C69",
-
-    },
-    containerButton: {
-        flex: 1,
-        width: "100%",
-    },
-    btntext: {
-        color: "#FFFFFF",
-        margin: 8,
-        textAlign: "center",
-    }
-})
-
-
-export default styles
+export default styles;
